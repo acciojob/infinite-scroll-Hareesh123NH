92554aaa43d8be183289bb82211bf56fb02fe36c
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-17 18:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here!
 
+const list=document.getElementById("infi-list");
+let item=1;
+function addItems(count) {
+	for (let i = 0; i < count; i++) {
+		const ele=document.createElement("li");
+		ele.innerText=`Item ${item++}`;
+		list.appendChild(ele);
+	}
+}
+
+addItems(10);
